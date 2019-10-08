f4d5504fb3d95ae0717b07f3346e60c17800bac6
orchid2ev <orchid@hyperorchid.org>
2019-10-08 09:14:34 +0800

    merge max

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+/**
+ * MetaMask Support
+ *  |\_/|,,_____,~~`
+ *  (.".)~~     )`~}}
+ *   \o/\ /---~\\ ~}}
+ *     _//    _// ~}
+ * 
+ * Copyright (c) 2019 hyperorchid.org,orchid2ev
+ * E-mail :orchid@hyperorchid.org
+ * git@flash:hyperorchidlab/website.git
+ * 
+ */
 (function($){
   'use strict';
 
