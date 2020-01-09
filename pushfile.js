cef5d7437bc3f673137ea46bb0b8437cba92e812
orchid2ev <hyperorchid-lab@gmail.com>
2020-01-09 19:57:34 +0800

    update upload file

--- pushfile.js
+++ pushfile.js
@@ -2,6 +2,7 @@ const pkgJson = require('./package.json')
 const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
+const os = require('os');
 const DateFormat = require('fast-date-format');
 
 const dateFormat = new DateFormat('YYDDDDhhmm');
@@ -41,11 +42,20 @@ const PUSH_FILE = process.env.PUSH_FILE
 console.log('PUSH_FILE>>',PUSH_FILE)
 
 //
-execMkdir();
+checkPushFile(PUSH_FILE);
+//
+const creatDir = execMkdir();
+
+//upload file
+
+if(creatDir){
+ let r =  execBash(uploadBash())
+}
 
 
 
 function preparedIEnv() {
+  console.log('OS>>',os.type())
   if(!IEnv['REMOTE_HOST'])throw 'need set REMOTE_HOST'
 
   if(typeof IEnv['SSH_KEY'] !== 'string')throw 'need set SSH_KEY'
@@ -57,9 +67,14 @@ function preparedIEnv() {
   IEnv['SSH_KEY_PATH'] = sshKey
   if(shell.find(sshKey).length <= 0)throw 'not fund SSH_KEY'
 
+  if(os.type()=='Windows_NT' && sshKey.indexOf(':') > 0){
+    let at = sshKey.indexOf(':');
+    sshKey = '\\'+sshKey.substring(0,at)  + sshKey.substring(at+1)
+
+    IEnv['SSH_KEY_PATH'] = sshKey.replace(/\\/g,'/')
+  }
 
   let now = new Date()
-
 
   IEnv['BAK_FOLDER'] = dateYMD.format(now)
   IEnv['BAK_SUFFIX'] = dateFormat.format(now)
@@ -79,14 +94,23 @@ function getCreateBackupFolderBash(){
 
   let bash = `mkdir -p ${IEnv.REMOTE_DEST_HOME}/${IEnv.BAK_FOLDER}`
 
-  return ssh +' '  + bash
+  return ssh +' \''  + bash+'\''
 }
 
 function getSSHPreFix(){
-  let str = `ssh -i ${IEnv.SSH_KEY_PATH} ${user}@${IEnv.REMOTE_HOST}`
+  let str = `ssh -i ${IEnv.SSH_KEY_PATH} `
 
-  if(IEnv.REMOTE_PORT) str =str + ':'+IEnv.REMOTE_PORT
 
+  if(IEnv.REMOTE_PORT) str =str + ` -p ${IEnv.REMOTE_PORT}`
+
+  str = str + ` ${user}@${IEnv.REMOTE_HOST}`
+
+  return str
+}
+
+function getSCPPreFix(){
+  let str = `scp -i ${IEnv.SSH_KEY_PATH} `
+  if(IEnv.REMOTE_PORT) str =str + ` -P ${IEnv.REMOTE_PORT} `
   return str
 }
 
@@ -95,4 +119,22 @@ function execMkdir() {
   console.log(bash)
   var execResult = shell.exec(bash,{silent:true,async:false}).stdout
   console.log('exec:',execResult)
+
+  return !execResult
+}
+
+function uploadBash(){
+  let bash = getSCPPreFix()
+
+  bash = bash + `` + ` ${user}@${IEnv.REMOTE_HOST}:${IEnv.REMOTE_DEST_HOME}/${IEnv.BAK_FOLDER}/`
+
+  return bash;
+}
+
+function execBash(bash) {
+  console.log(bash)
+  var execResult = shell.exec(bash,{silent:true,async:false}).stdout
+  console.log('exec:',execResult)
+
+  return !execResult  
 }
